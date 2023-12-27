ba892f0757d0c51528cf156459dad2414579434e
nikkyeya-jsm <nikky@jsmastery.pro>
2023-12-28 04:53:42 +0800

    update clerk route

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,8 +1,9 @@
 /* eslint-disable camelcase */
-import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 import { headers } from "next/headers";
-import { WebhookEvent, clerkClient } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { clerkClient } from "@clerk/nextjs";
 
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 
