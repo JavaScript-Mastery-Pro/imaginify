5cb669ae8fb3771e081837c8f45c15988826fdbc
nikkyeya-jsm <nikky@jsmastery.pro>
2023-12-28 03:44:27 +0800

    add debug

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhook/clerk"],
   ignoredRoutes: ["/api/webhook/clerk"],
+  debug: true,
 });
 
 export const config = {
